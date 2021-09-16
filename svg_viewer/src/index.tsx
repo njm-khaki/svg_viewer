bcb0e06619e1d6e449aa6b915b93ae4edfee5926
njm-khaki <39180345+s1523119@users.noreply.github.com>
2021-09-16 20:26:23 +0900

    Reactプロジェクト作成

---
+++ svg_viewer/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Viewer } from './viewer/viewer'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Viewer />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
